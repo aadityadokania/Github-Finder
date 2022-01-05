4a9fa20d40db4e4035eb06d6b848ba1ace58b80e
Aaditya Dokania <94733541+aadityadokania@users.noreply.github.com>
2022-01-05 22:27:41 +0530

    added the ui js class and other functions

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 //init github
 const github = new Github;
 
+//init UI
+const ui = new UI;
+
 const searchUser = document.getElementById('searchUser');
 
 
@@ -9,14 +12,27 @@ const searchUser = document.getElementById('searchUser');
 searchUser.addEventListener('keyup',(e) => {
     //get input text
     const userText = e.target.value;
+    
 
     if(userText !== ''){
         
         //Make http call
         github.getUser(userText)
         .then(data=>{
-            console.log(data);
+            if(data.profile.message === 'Not Found' ){
+                //show alert
+                ui.showAlert('User Not Found','alert alert-danger');
+
+            }else{
+                //show profile
+                // console.log("he");
+                ui.showProfile(data.profile);
+
+            }
         })
 
+    }else{
+        //clear profile 
+        ui.clearProfile();
     }
 })
