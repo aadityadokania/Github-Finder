733c89b5cc3c3173fef4873e2dc9cdc0818cca65
Aaditya Dokania <94733541+aadityadokania@users.noreply.github.com>
2022-01-06 21:29:58 +0530

    added show repos

--- ui.js
+++ ui.js
@@ -13,10 +13,10 @@ class UI {
                     class="btn btn-primary btn-block">View Profile</a>
                 </div>
             <div class="col-md-9">
-        <span class="badge badge-primary"> Public Repos:${user.public_repos}</span>
-        <span class="badge badge-secondary"> Public Gists:${user.public_gists}</span>
-        <span class="badge badge-sucess"> Followers:${user.followers}</span>
-        <span class="badge badge-info"> Following:${user.following}</span>
+        <span class="badge bg-primary"> Public Repos:${user.public_repos}</span>
+        <span class="badge bg-secondary"> Public Gists:${user.public_gists}</span>
+        <span class="badge bg-success"> Followers:${user.followers}</span>
+        <span class="badge bg-info"> Following:${user.following}</span>
         </br>
         </br>
         <ul class="list-group">
@@ -31,9 +31,35 @@ class UI {
 
         </div>
         </div>
-        <h3 class="page-heading mb-3>Latest Repos</h3>
+        <h3 class="page-heading mb-3">Latest Repos</h3>
         <div id="repos"></div>
                 `;
+  }
+
+  showRepos(repos){
+        let output = '';
+        repos.forEach((repo)=>{
+                output += `
+                    <div class = "card card-body mb-2">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                        </div>
+
+                        <div class="col-md-6">
+                        <span class="badge bg-primary"> Stars:${repo.stargazers_count}</span>
+                        <span class="badge bg-secondary"> Watchers:${repo.watchers_count}</span>
+                        <span class="badge bg-success"> Forks:${repo.forks_count}</span>
+                        
+                        </div>
+                    </div>
+
+                    </div>
+                `;
+        });
+
+        //output repos
+       document.getElementById('repos').innerHTML = output;
   }
 
   clearProfile(){
@@ -59,14 +85,14 @@ class UI {
 
     //Remove after 3 seconds
     setTimeout(()=>{
-        this.clearAlert
+        this.clearAlert()
     },3000);
 }
 
     clearAlert(){
         const currentAlert = document.querySelector('.alert');
         if(currentAlert){
-            currentAlert.remove;
+            currentAlert.remove();
         }
     }
 }
