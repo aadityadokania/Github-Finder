0ddfb9b3dcd02df2dd007447da615e2d0f5c6060
Aaditya Dokania <94733541+aadityadokania@users.noreply.github.com>
2022-01-06 21:30:19 +0530

    Update github.js

--- github.js
+++ github.js
@@ -4,16 +4,23 @@ class Github {
 
     constructor(){
         this.client_id = 'e6ed661a3dbd3ef715ff';
-        this.client_secret = 'c21ef23a87c13ada7c1add24e2e19c8698a555eb';
+        this.client_secret = 'ebbb896af8409d1b5e25c6f9403976de47f9d65a';
+        this.repo_count = 5;
+        this.repo_sort = "created: asc";
     }
 
     async getUser(user){
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repo_count}&sort=${this.repo_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+
         const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
 
         return{
-            profile : profile
+            profile : profile,
+            repos : repos
         }
     }
 }
